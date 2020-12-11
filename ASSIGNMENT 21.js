c9c8b38e53163b5fdba0c345b076c25a42386b1a
Saksham Agarwal <sakshamagarwal330@gmail.com>
2020-12-11 23:07:01 +0530

    Add files via upload

---
+++ ASSIGNMENT 21.js
@@ -0,0 +1,221 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+
+function googleTranslateElementInit() { 
+  new google.translate.TranslateElement( 
+    {pageLanguage: 'en'}, 
+    'google_translate_element' 
+  ); 
+} 
+
+alert("FOR CUBE AREA");
+a=prompt("Enter a value for side:");
+alert(a*a*a);
+alert("FOR CUBOID'S AREA");
+b=prompt("Enter a value for length:");
+c=prompt("Enter a value for Breadth:");
+d=prompt("Enter a value for Height:");
+alert(2*b*c + 2*c*d + 2*b*d);
+alert("FOR AREA OF TRIANGLE");
+e=prompt("Enter a value for Breadth:");
+f=prompt("Enter a value for Height:");
+alert(0.5*e*f);
+alert("FOR CIRCLE");
+g=prompt("Enter a value for radius:");
+alert(2*3.14*e);
+
+
+function openpdf1() {
+   var x = document.getElementById("myFrame1");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "Areas.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf2() {
+   var x = document.getElementById("myFrame2");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "volumes.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf3() {
+   var x = document.getElementById("myFrame3");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "functions-equations.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf4() {
+   var x = document.getElementById("myFrame4");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "exponents.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf5() {
+   var x = document.getElementById("myFrame5");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "radicals.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf6() {
+   var x = document.getElementById("myFrame6");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "trigonometry.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf7() {
+   var x = document.getElementById("myFrame7");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "geometry.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf8() {
+   var x = document.getElementById("myFrame8");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "logic.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf9() {
+   var x = document.getElementById("myFrame9");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "vectors.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf10() {
+   var x = document.getElementById("myFrame10");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "statistic.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf11() {
+   var x = document.getElementById("myFrame11");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "sequences.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf12() {
+   var x = document.getElementById("myFrame12");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "derivatives.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf13() {
+   var x = document.getElementById("myFrame13");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "probability-sets.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf14() {
+   var x = document.getElementById("myFrame14");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "logarithms.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf15() {
+   var x = document.getElementById("myFrame15");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "special-limits.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf16() {
+   var x = document.getElementById("myFrame16");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "integrals-primitives.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+ 
+ function openpdf17() {
+   var x = document.getElementById("myFrame17");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+      x.src = "complex-numbers.pdf";
+   }
+    else {
+     x.style.display = "none";
+   }
+ }
+
